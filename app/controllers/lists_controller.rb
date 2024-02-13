e8d5b10461b8e79d6cd1f461f22f9a81d98a0694
Eleanor Vazraghi <eleanorvazraghi@gmail.com>
2024-02-13 21:32:26 +0000

    hi

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,19 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
-    @lists = List.all
+    if params[:query].present?
+      @query = params[:query]
+      @lists = List.where("name ILIKE ?","%#{params[:query]}%")
+    else
+      @lists = List.all
+    end
   end
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @review = Review.new  #(list: @list)
   end
 
   def new
@@ -14,20 +23,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list), :notice => "New list added."
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def set_list
-    @list = List.find(params[:list_id])
+    @list = List.find(params[:id])
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
-
 end
